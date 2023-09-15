5e134937022d9b6c8091ba3be4fbfc1ef2ceba05
Sibyl-null <1286438810@qq.com>
2023-09-15 23:56:47 +0800

    [feat]: Window窗口抽象，WindowsWindow实现

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,11 +1,13 @@
 #include "hzpch.h"
 #include "Application.h"
 #include "Hazel/Events/ApplicationEvent.h"
-#include "Hazel/Log.h"
+
+#include <GLFW/glfw3.h>
 
 namespace Hazel {
 	Application::Application()
 	{
+		m_Window = std::unique_ptr<Window>(Window::Create());
 	}
 
 	Application::~Application()
@@ -14,11 +16,10 @@ namespace Hazel {
 
 	void Application::Run()
 	{
-		WindowResizeEvent e(1280, 720);
-		if (e.IsInCategory(EventCategoryApplication)) {
-			HZ_TRACE(e);
+		while (m_Running) {
+			glClearColor(0.3, 0.3, 0, 0);
+			glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
 		}
-
-		while (true){}
 	}
 }
---
+++ Hazel/src/Platform/Windows/WindowsWindow.cpp
@@ -0,0 +1,72 @@
+#include "hzpch.h"
+#include "WindowsWindow.h"
+
+namespace Hazel {
+	static bool s_GLFWInitialized = false;
+
+	Window* Window::Create(const WindowProps& props) {
+		return new WindowsWindow(props);
+	}
+
+	WindowsWindow::WindowsWindow(const WindowProps& props)
+	{
+		Init(props);
+	}
+
+	WindowsWindow::~WindowsWindow()
+	{
+		Shutdown();
+	}
+
+	void WindowsWindow::Init(const WindowProps& props)
+	{
+		m_Data.Title = props.Title;
+		m_Data.Width = props.Width;
+		m_Data.Height = props.Height;
+
+		HZ_CORE_INFO("Creating window {0} ({1}, {2})", props.Title, props.Width, props.Height);
+
+		if (s_GLFWInitialized == false) {
+			int success = glfwInit();
+			HZ_CORE_ASSERT(success, "Could not intialize GLFW!");
+
+			s_GLFWInitialized = true;
+		}
+
+		m_Window = glfwCreateWindow((int)m_Data.Width, (int)m_Data.Height, m_Data.Title.c_str(), nullptr, nullptr);
+		glfwMakeContextCurrent(m_Window);
+		// 将自定义的数据与窗口对象关联起来，并在需要时通过窗口对象来获取该数据
+		glfwSetWindowUserPointer(m_Window, &m_Data);
+		SetVSync(true);
+	}
+
+	void WindowsWindow::Shutdown()
+	{
+		glfwDestroyWindow(m_Window);
+	}
+
+	void WindowsWindow::OnUpdate()
+	{
+		glfwPollEvents();
+		glfwSwapBuffers(m_Window);
+	}
+
+	void WindowsWindow::SetVSync(bool enabled)
+	{
+		// glfwSwapInterval 用于设置垂直同步的交换间隔。
+		// 值为 0 将禁用垂直同步，glfwSwapBuffers 调用会立即返回。
+		// 值大于 0，glfwSwapBuffers 调用将会在等待恰好 x 个垂直同步信号之后再返回.
+
+		if (enabled)
+			glfwSwapInterval(1);
+		else
+			glfwSwapInterval(0);
+
+		m_Data.VSync = enabled;
+	}
+
+	bool WindowsWindow::IsVSync() const
+	{
+		return m_Data.VSync;
+	}
+}
