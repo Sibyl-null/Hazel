a9619622cab1d3cec774fcb04e210a7eabe76506
Sibyl-null <1286438810@qq.com>
2023-09-19 23:49:04 +0800

    [feat]: ImGui层级，渲染Demo

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -16,12 +16,17 @@ namespace Hazel {
 
 		void PushLayer(Layer* layer);
 		void PushOverlay(Layer* layer);
+
+		inline Window& GetWindow() { return *m_Window; }
+		inline static Application& Get() { return *s_Instance; }
 	private:
 		bool OnWindowClose(WindowCloseEvent& e);
 	private:
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 		LayerStack m_LayerStack;
+
+		static Application* s_Instance;
 	};
 
 	// 在客户端定义
