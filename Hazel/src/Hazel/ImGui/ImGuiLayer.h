ca7474038ed8944ce4007143fee05d7fbe5a6455
Sibyl-null <1286438810@qq.com>
2023-10-04 19:30:32 +0800

    [feat]: ImGuiLayer去除Demo显示

--- Hazel/src/Hazel/ImGui/ImGuiLayer.h
+++ Hazel/src/Hazel/ImGui/ImGuiLayer.h
@@ -1,18 +1,14 @@
 #pragma once
 #include "Hazel/Core/Layer.h"
-#include "Hazel/Events/KeyEvent.h"
-#include "Hazel/Events/MouseEvent.h"
-#include "Hazel/Events/ApplicationEvent.h"
 
 namespace Hazel {
-	class HAZEL_API ImGuiLayer : public Layer{
+	class ImGuiLayer : public Layer{
 	public:
 		ImGuiLayer();
-		~ImGuiLayer();
+		~ImGuiLayer() override;
 
 		void OnAttach() override;
 		void OnDetach() override;
-		void OnImGuiRender() override;
 
 		void Begin();
 		void End();
