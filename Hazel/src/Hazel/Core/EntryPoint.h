33973581eb08f8f2f63f73e0bbc9821f4ead3051
Sibyl-null <1286438810@qq.com>
2023-10-04 20:21:05 +0800

    [feat]: 函数耗时图表工具

--- Hazel/src/Hazel/Core/EntryPoint.h
+++ Hazel/src/Hazel/Core/EntryPoint.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Hazel/Debug/Instrumentor.h"
 
 #ifdef HZ_PLATFORM_WINDOWS
 
@@ -8,9 +9,17 @@ int main(int argc, char** argv)
 {
 	Hazel::Log::Init();
 
+	HZ_PROFILE_BEGIN_SESSION("Startup", "HazelProfile-Startup.json");
 	auto app = Hazel::CreateApplication();
+	HZ_PROFILE_END_SESSION();
+
+	HZ_PROFILE_BEGIN_SESSION("Runtime", "HazelProfile-Runtime.json");
 	app->Run();
+	HZ_PROFILE_END_SESSION();
+
+	HZ_PROFILE_BEGIN_SESSION("Startup", "HazelProfile-Shutdown.json");
 	delete app;
+	HZ_PROFILE_END_SESSION();
 }
 
 #endif
