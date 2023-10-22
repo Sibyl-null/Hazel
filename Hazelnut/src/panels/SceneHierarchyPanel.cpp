e306f523b07fc4e6732fdd2ac59344911b96e023
Sibyl-null <1286438810@qq.com>
2023-10-22 22:38:23 +0800

    [feat]: Component Properties 窗口实现

--- Hazelnut/src/panels/SceneHierarchyPanel.cpp
+++ Hazelnut/src/panels/SceneHierarchyPanel.cpp
@@ -1,5 +1,6 @@
 ﻿#include "SceneHierarchyPanel.h"
 #include "imgui/imgui.h"
+#include <glm/gtc/type_ptr.hpp>
 
 namespace Hazel
 {
@@ -16,13 +17,23 @@ namespace Hazel
     void SceneHierarchyPanel::OnImGuiRender()
     {
         ImGui::Begin("Scene Hierarchy");
-
-        m_Context->m_Registry.each([&](entt::entity entityId)
         {
-            const Entity entity{ entityId, m_Context.get() };
-            DrawEntityNode(entity);
-        });
+            m_Context->m_Registry.each([&](entt::entity entityId)
+            {
+                const Entity entity{ entityId, m_Context.get() };
+                DrawEntityNode(entity);
+            });
+
+            if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered())
+                m_SelectionContext = {};
+        }
+        ImGui::End();
         
+        ImGui::Begin("Properties");
+        {
+            if (m_SelectionContext)
+                DrawComponents(m_SelectionContext);
+        }
         ImGui::End();
     }
 
@@ -45,6 +56,32 @@ namespace Hazel
             if (op)
                 ImGui::TreePop();
             ImGui::TreePop();
+        }
+    }
+
+    void SceneHierarchyPanel::DrawComponents(Entity entity)
+    {
+        if (entity.HasComponent<TagComponent>())
+        {
+            std::string& tag = entity.GetComponent<TagComponent>().Tag;
+
+            char buffer[256] = {};
+            strcpy_s(buffer, sizeof(buffer), tag.c_str());
+
+            if (ImGui::InputText("Tag", buffer, sizeof(buffer)))
+                tag = std::string(buffer);
+        }
+
+        if (entity.HasComponent<TransformComponent>())
+        {
+            if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(),
+                ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
+            {
+                auto& transform = entity.GetComponent<TransformComponent>().Transform;
+                ImGui::DragFloat3("Position", glm::value_ptr(transform[3]), 0.1f);
+
+                ImGui::TreePop();
+            }
         }
     }
 }
